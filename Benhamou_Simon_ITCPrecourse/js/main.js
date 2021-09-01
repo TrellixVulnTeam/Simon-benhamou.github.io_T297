996992e6050eff15776a077571f432c36786a3a0
Simon benhamou <84936931+Simon-benhamou@users.noreply.github.com>
2021-09-01 15:30:34 +0300

    update

--- Benhamou_Simon_ITCPrecourse/js/main.js
+++ Benhamou_Simon_ITCPrecourse/js/main.js
@@ -1,11 +1,11 @@
 //replace with your user:
-const GITHUB_URL = "https://github.com/Simon-benhamou";
+const GITHUB_URL = "https://api.github.com/users/Simon-benhamou";
 
 fetch(GITHUB_URL)
   .then(function(response) {
     return response.json();
   })
   .then(function (data) {
-    const profileImage = document.getElementById("Simon profile-image");
+    const profileImage = document.getElementById("profile-image");
     profileImage.src = data.avatar_url;
   });
