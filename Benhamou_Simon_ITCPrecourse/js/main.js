498370c691b945ddce59bdf97e62c062e59f8433
Simon benhamou <84936931+Simon-benhamou@users.noreply.github.com>
2021-09-05 20:55:46 +0300

    update

--- Benhamou_Simon_ITCPrecourse/js/main.js
+++ Benhamou_Simon_ITCPrecourse/js/main.js
@@ -1,6 +1,12 @@
 //replace with your user:
 const GITHUB_URL = "https://api.github.com/users/Simon-benhamou";
 
+const sentence=document.querySelector('#used-language');
+const languages=["HTML","CSS","Javascript"];
+var last = languages.pop();
+var string =  sentence.innerText + languages.join(', ')+ " and " + last +".";
+sentence.innerText = string;
+
 fetch(GITHUB_URL)
   .then(function(response) {
     return response.json();
@@ -10,5 +16,5 @@ fetch(GITHUB_URL)
     profileImage.src = data.avatar_url;
     const profileName = document.querySelector('#myName');
     profileName.innerHTML= data.name;
-  });
   
+  });
